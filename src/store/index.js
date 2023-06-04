2e19979dbab1a892f3bb06c17c681be55289fdec
Ajith <ajithpkumar231@gmail.com>
2023-06-04 14:14:58 +0530

    learned using redux-toolkit

--- src/store/index.js
+++ src/store/index.js
@@ -1,19 +1,50 @@
-import { legacy_createStore as createStore } from "redux";
+import { configureStore, createSlice } from "@reduxjs/toolkit";
 
-const reducerFn = (state = { counter: 10 }, action) => {
-  // Synchronous Function
-  // We should not mutate the original state (Always create a copy of the original state and do changes to it. The original one should remain intact)
+const counterSlice = createSlice({
+  name: "counter",
+  initialState: { counter: 0 },
+  reducers: {
+    increment(state, action) {
+      state.counter++;
+    },
+    decrement(state, action) {
+      state.counter--;
+    },
+    addBy(state, action) {
+      state.counter += action.payload;
+    },
+  },
+});
 
-  if (action.type === "INC") {
-    return { counter: state.counter + 1 };
-  }
-  if (action.type === "DEC") {
-    return { counter: state.counter - 1 };
-  }
+export const actions = counterSlice.actions;
 
-  return state;
-};
-
-const store = createStore(reducerFn);
+const store = configureStore({
+  reducer: counterSlice.reducer,
+});
 
 export default store;
+
+// First redux code
+
+// import { legacy_createStore as createStore } from "redux";
+
+// const reducerFn = (state = { counter: 0 }, action) => {
+//   // Synchronous Function
+//   // We should not mutate the original state (Always create a copy of the original state and do changes to it. The original one should remain intact)
+
+//   if (action.type === "INC") {
+//     return { counter: state.counter + 1 };
+//   }
+//   if (action.type === "DEC") {
+//     return { counter: state.counter - 1 };
+//   }
+//   if (action.type === "ADD") {
+//     return { counter: state.counter + action.payload };
+//   }
+
+//   return state;
+// };
+
+// const store = createStore(reducerFn);
+
+// export default store;
